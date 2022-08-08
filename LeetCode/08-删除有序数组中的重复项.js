c06c31819b6d1c5e3dab56337c3fe03df86a198c
yanyue <yanyue@actionsky.com>
2022-08-08 17:28:35 +0800

    原地删除有序数组的重复项

---
+++ LeetCode/08-删除有序数组中的重复项.js
@@ -0,0 +1,35 @@
+/**
+ * 给你一个 升序排列 的数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，
+ * 返回删除后数组的新长度。元素的 相对顺序 应该保持 一致 。
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// 自己的解法:遍历数组,如果有相同的,删除数据，然后递归
+var removeDuplicates = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === nums[i + 1]) {
+      nums.splice(i, 1);
+      removeDuplicates(nums);
+    }
+  }
+  return nums.length;
+};
+// 官方题解：双指针
+var removeDuplicates = function (nums) {
+  const n = nums.length;
+  if (n === 0) {
+    return 0;
+  }
+  let fast = 1,
+    slow = 1;
+  while (fast < n) {
+    if (nums[fast] !== nums[fast - 1]) {
+      nums[slow] = nums[fast];
+      ++slow;
+    }
+    ++fast;
+  }
+  return slow;
+};
