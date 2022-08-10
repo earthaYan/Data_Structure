a7fa05ce98197db0a7cd21728ef898e7e09a7da8
yanyue <yanyue@actionsky.com>
2022-08-10 15:52:03 +0800

    修改最大正方形报错

--- LeetCode/11-最大正方形.js
+++ LeetCode/11-最大正方形.js
@@ -7,27 +7,27 @@ var maximalSquare = function (matrix) {
   let maxRow = matrix.length;
   let maxCol = matrix[0].length;
   let maxSide = 0;
-  if (maxRow === 0 || maxCol === 0) {
+  if (!matrix || maxRow === 0 || maxCol === 0) {
     return 0;
   }
-  for (let row in matrix) {
-    let currentRow = matrix[row];
-    for (let col in currentRow) {
-      if (currentRow[col] === '1') {
-        // 遇到一个 1 作为正方形的左上角
+  for (let row = 0; row < maxRow; row++) {
+    for (let col = 0; col < maxCol; col++) {
+      // 找到正方形左上角
+      if (matrix[row][col] === '1') {
         maxSide = Math.max(maxSide, 1);
         // 计算可能的最大正方形边长
         let currentMaxSide = Math.min(maxRow - row, maxCol - col);
+        // 判断新增的一行一列是否均为 1
         for (let k = 0; k < currentMaxSide; k++) {
-          // 判断新增的一行一列是否均为 1
           let flag = true;
-          if (matrix[row + k][col + k] == '0') {
+          if (matrix[row + k][col + k] === '0') {
             break;
           }
           for (let m = 0; m < k; m++) {
+            // 判断当前是否是正方形边界线
             if (
-              matrix[row + k][col + m] == '0' ||
-              matrix[row + m][col + k] == '0'
+              matrix[row + k][col + m] === '0' ||
+              matrix[row + m][col + k] === '0'
             ) {
               flag = false;
               break;
@@ -42,9 +42,7 @@ var maximalSquare = function (matrix) {
       }
     }
   }
-  let maxSquare = Math.pow(maxSide, 2);
-  console.log(maxSide, maxSquare);
-  return maxSquare;
+  return Math.pow(maxSide, 2);
 };
 maximalSquare([
   ['1', '0', '1', '0', '0'],
