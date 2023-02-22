e8e6afdda677da90a487c70ab370ec1360e78238
yanyue <yanyue@actionsky.com>
2023-02-22 14:12:42 +0800

    del useless code

--- main.go
+++ main.go
@@ -1,13 +1,10 @@
 package main
 
-import "leet_code_go/LeetCode_GoLang"
-
 func main() {
 
 }
 func init() {
 	// digits := []int{26,2,16,16,5,5,26,2,5,20,20,5,2,20,2,2,20,2,16,20,16,17,16,2,16,20,26,16}
 	// s := "   fly me   to   the moon  "
-	s := "LVIII"
-	LeetCode_GoLang.RomanToInt(s)
+
 }
